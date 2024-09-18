24a5109b2b9e009a0cb6ff319b95598b40276810
Sansan Fajrian <sansan.fajrian@gmail.com>
2024-09-18 12:54:17 +0700

    controler.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Laravel\Passport\TokenRepository;
+use Laravel\Passport\ClientRepository;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Token\Builder;
+
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected static function getCurrentToken($request)
+	{
+	    $tokenRepository = new TokenRepository();
+	    $token = app(Parser::class)->parse($request->bearerToken())->claims()->get('jti');
+
+	    return $token;
+	}
 }
